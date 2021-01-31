d7311decc875d0b6783d19a55139105cca8f3f6a
ANGELLOZ-NICOUD Sébastien <sebastien.angelloz@gmail.com>
2021-02-01 00:36:14 +0100

    Ajout du forward vers la page d'accueil si pas de saisie index.html Gestion des urls angular.

--- src/main/java/aesahaettr/AesahaettrApplication.java
+++ src/main/java/aesahaettr/AesahaettrApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
@@ -14,7 +15,7 @@ public class AesahaettrApplication {
     }
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer webMvcConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
@@ -22,6 +23,13 @@ public class AesahaettrApplication {
                         .allowedOrigins("http://localhost:4200", "http://stelmaria:8080")
                         .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
             }
+
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("forward:/index.html");
+                registry.addViewController("{path:(?:(?!api|\\.).)*}/**").setViewName("forward:/");
+            }
+
         };
     }
 
